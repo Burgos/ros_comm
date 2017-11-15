eacc75027f20a5239a25363d40b5c74e16063e21
Mike Purvis <mpurvis@clearpathrobotics.com>
2017-11-15 14:27:47 -0500

    Implement Bag encryption/decryption. (#1206)
    Ported to 1.12.12

--- tools/rosbag_storage/include/rosbag/bag.h
+++ tools/rosbag_storage/include/rosbag/bag.h
@@ -40,6 +40,7 @@
 #include "rosbag/buffer.h"
 #include "rosbag/chunked_file.h"
 #include "rosbag/constants.h"
+#include "rosbag/encryptor.h"
 #include "rosbag/exceptions.h"
 #include "rosbag/structures.h"
 
@@ -59,6 +60,8 @@
 
 #include <boost/format.hpp>
 #include <boost/iterator/iterator_facade.hpp>
+
+#include <pluginlib/class_loader.h>
 
 #include "console_bridge/console.h"
 // Remove this when no longer supporting platforms with libconsole-bridge-dev < 0.3.0,
@@ -139,6 +142,16 @@ public:
     CompressionType getCompression() const;                       //!< Get the compression method to use for writing chunks
     void            setChunkThreshold(uint32_t chunk_threshold);  //!< Set the threshold for creating new chunks
     uint32_t        getChunkThreshold() const;                    //!< Get the threshold for creating new chunks
+
+    //! Set encryptor of the bag file
+    /*!
+     * \param plugin_name The name of the encryptor plugin
+     * \param plugin_param The string parameter to be passed to the plugin initialization method
+     *
+     * Call this method to specify an encryptor for writing bag contents. This method need not be called when
+     * reading or appending a bag file: The encryptor is read from the bag file header.
+     */
+    void setEncryptorPlugin(const std::string& plugin_name, const std::string& plugin_param = std::string());
 
     //! Write a message into the bag file
     /*!
@@ -331,6 +344,11 @@ private:
     mutable Buffer*  current_buffer_;
 
     mutable uint64_t decompressed_chunk_;      //!< position of decompressed chunk
+
+    // Encryptor plugin loader
+    pluginlib::ClassLoader<rosbag::EncryptorBase> encryptor_loader_;
+    // Active encryptor
+    boost::shared_ptr<rosbag::EncryptorBase> encryptor_;
 };
 
 } // namespace rosbag
